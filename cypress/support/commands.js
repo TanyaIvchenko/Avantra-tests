1b3bb8d1bb2d32543a85f628c00d5bebd4aa9918
AlexanderWuermlin <alex.wuermlin@avantra.com>
2021-10-20 11:05:47 +0200

    Added command delay to command.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -55,5 +55,22 @@ Cypress.Commands.add("webdriverUni_ContactForm_Submission", (firstName, lastName
     cy.get($selector).contains(textToLocate)
 })
 
+// Set CYPRESS_COMMAND_DELAY above zero for demoing to stakeholders,
+// E.g. CYPRESS_COMMAND_DELAY=1000 node_modules/.bin/cypress open
+const COMMAND_DELAY = Cypress.env('COMMAND_DELAY') || 2000;
+if (COMMAND_DELAY > 0) {
+    for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', 'contains']) {
+        Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+            const origVal = originalFn(...args);
+
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(origVal);
+                }, COMMAND_DELAY);
+            });
+        });
+    }
+}
+
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
