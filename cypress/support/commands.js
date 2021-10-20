edf8ad6247f70605af3426d38f1f07c65fc56be1
AlexanderWuermlin <alex.wuermlin@avantra.com>
2021-10-20 14:52:40 +0200

    Changed the delay after each command to 0ms

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -57,7 +57,7 @@ Cypress.Commands.add("webdriverUni_ContactForm_Submission", (firstName, lastName
 
 // Set CYPRESS_COMMAND_DELAY above zero for demoing to stakeholders,
 // E.g. CYPRESS_COMMAND_DELAY=1000 node_modules/.bin/cypress open
-const COMMAND_DELAY = Cypress.env('COMMAND_DELAY') || 2000;
+const COMMAND_DELAY = Cypress.env('COMMAND_DELAY') || 0;
 if (COMMAND_DELAY > 0) {
     for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', 'contains']) {
         Cypress.Commands.overwrite(command, (originalFn, ...args) => {
