5754a4c71296ef9f85094d2377d657d23d6eba94
Tanya <tatyana.ivchenko@avantra.com>
2021-11-17 14:29:06 +0200

    studio and cucumber

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -12,16 +12,9 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-require('cypress-plugin-retries')
-require('cypress-xpath')
 
 // Import commands.js using ES2015 syntax:
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-Cypress.Server.defaults({
-    whitelist: (xhr) => {
-        return true;
-    }
-})
