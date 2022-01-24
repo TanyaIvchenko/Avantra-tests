64246af61ee7ec986eb7f1b513c91282c4e719a1
Olha <vicontesse@gmail.com>
2022-01-24 18:00:46 +0200

    OLS Dashlets file

--- cypress/integration/Avantra UI/Dashlets.js
+++ cypress/integration/Avantra UI/Dashlets.js
@@ -11,11 +11,77 @@ describe("Dashlets", () => {
     })
 
 
-it("Login UI without checkbox", () => {
+it("Save the dashboard with dashlet added", () => {
     cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text','Dashboards')
+    cy.get('.sidebar-list__header > app-button.ng-star-inserted > .icon-button > .icon-button__control > svg').click();
+    cy.get('.dashboard-modify__header-input').clear();
+    cy.get('.dashboard-modify__header-input').type('OLS4');
+    cy.get('.dashboard-modify__add-dashlet').click();
+    cy.get(':nth-child(1) > app-dashlet-selector-item > .dashlet-selector-item > .dashlet-selector-item__button').click();
+    cy.get('.header__edit-block > app-button.ng-star-inserted > .icon-button > .icon-button__control > svg').click();
+    cy.wait(3000)
+    //get element within another element
+    cy.get('[iconpath="assets/media/icons/shared/menu-ok.svg"]').within(() => {
+        cy.get('[type="button"]').click()
+    })
+});
 
+
+    it("Cancel delete and submit delete of the dashboard", () => {
+//Cancel
+    cy.contains('a', 'OLS3').parent('[class="sidebar-list-item"]').within(() => {
+    cy.get('[class="mat-menu-trigger sidebar-list-item__menu-button ng-star-inserted"]').click({force:true})
+    })
+    //click Delete on menu appered
+    cy.get('[iconpath="assets/media/icons/shared/menu-remove.svg"]').click()
+    //pop-up confirmation: cancel
+    cy.get('[class="confirmation-modal"]').within(() => {
+        cy.contains('No').click()
+    })
+//Delete
+    cy.contains('a', 'OLS3').parent('[class="sidebar-list-item"]').within(() => {
+        cy.get('[class="mat-menu-trigger sidebar-list-item__menu-button ng-star-inserted"]').click({force:true})
+    })
+    //click Delete on menu appered
+    cy.get('[iconpath="assets/media/icons/shared/menu-remove.svg"]').click()
+    //pop-up confirmation: confirm deletion
+    cy.get('[class="confirmation-modal"]').within(() => {
+        cy.contains('Yes').click()
+    })
+//Verify the dashboard is deleted
+cy.contains('a', 'OLS3').should('not.exist')
         });
 
+//Dashlets selecting
+        it.only("Administrative Dashlets", () => {
+            cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text','Dashboards')
+            cy.get('.sidebar-list__header > app-button.ng-star-inserted > .icon-button > .icon-button__control > svg').click();
+            cy.get('.dashboard-modify__header-input').clear();
+            cy.get('.dashboard-modify__header-input').type('Administrative-ols');
+            cy.get('.dashboard-modify__add-dashlet').click();
+            cy.wait(2000)
+    //Log each element of dashlet categories
+            cy.get('[class="dashlet-selector-categories"]').within(() => {
+                cy.get('[class="dashlet-selector-categories__item ng-star-inserted"]').each(($el) =>{
+                    cy.get($el).invoke('text').then(cy.log)
+                })
+
+            })
+            cy.contains('Administrative').click()
+    //verify the correct category entered
+            cy.get('.dashlet-selector__sub-title').should('have.text', "Administrative Dashlets")
+    //Verify the number of items
+            cy.get('.dashlet-selector-item__title').should('have.length', 4)
+    //Verify the list of dashlet names
+            cy.get('.dashlet-selector-item__title').then(($els) => {
+               return (
+               Cypress.$.makeArray($els)
+               .map((el) => el.innerText)
+               )
+            })
+            .should('deep.equal', ['Signed in Users', 'Check For Updates', 'Logbook Activities', 'Business Service Node'])
+     
+        });
     
 
 
