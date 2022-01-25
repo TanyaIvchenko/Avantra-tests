7a52efb1c57cfdfa4bd47eb23aad778018f59dc1
Olha <vicontesse@gmail.com>
2022-01-25 17:14:19 +0200

    More JSON for dashlets

--- cypress/integration/Avantra UI/Dashboards_Dashlets_ols.js
+++ cypress/integration/Avantra UI/Dashboards_Dashlets_ols.js
@@ -3,17 +3,17 @@
 describe("Dashlets and dashboards", () => {
     before (function(){
         cy.fixture("Admin_dashlets").as("admDashJson")
+        cy.fixture("Credentials").as("creds")
     })
     beforeEach(() => {
-            const login = "Olha_test";
-            const password = "Olha1605";
-            
-        cy.visit ("https://eiger.dev.gcp.avantra.net:8443/xn/ui")
-        cy.get('#input-login-id').type(login)
-        cy.get('#input-password-id').type(password)
-        cy.get('.background-primary').contains("Login to Avantra").click()
+// DO NOT FORGET TO USE YOUR CREDS!!!!!!
+        cy.get("@creds").then((creds) =>{      
+            cy.visit ("https://eiger.dev.gcp.avantra.net:8443/xn/ui")
+            cy.get('#input-login-id').type(creds.login)
+            cy.get('#input-password-id').type(creds.password)
+            cy.get('.background-primary').contains("Login to Avantra").click()
+        })
     })
-
 
 it("Save the dashboard with dashlet added", () => {
     cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text','Dashboards')
@@ -58,43 +58,44 @@ it("Save the dashboard with dashlet added", () => {
 
 //Dashlets selecting
         it.only("Administrative Dashlets", () => {
-            const dashName = 'Administrative'
-            cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text','Dashboards')
-            cy.get('.sidebar-list__header > app-button.ng-star-inserted > .icon-button > .icon-button__control > svg').click();
-            cy.get('.dashboard-modify__header-input').clear();
-            cy.get('.dashboard-modify__header-input').type(dashName);
-            cy.get('.dashboard-modify__add-dashlet').click();
-            cy.wait(600)
+            cy.get("@admDashJson").then((admDashJson) =>{
+            
+                cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text','Dashboards')
+                cy.get('.sidebar-list__header > app-button.ng-star-inserted > .icon-button > .icon-button__control > svg').click();
+                cy.get('.dashboard-modify__header-input').clear();
+                cy.get('.dashboard-modify__header-input').type(admDashJson[0].type[0]);
+                cy.get('.dashboard-modify__add-dashlet').click();
+                cy.wait(600)
     //Log each element of dashlet categories
-            cy.get('[class="dashlet-selector-categories"]').within(() => {
+                cy.get('[class="dashlet-selector-categories"]').within(() => {
                     cy.get('[class="dashlet-selector-categories__item ng-star-inserted"]').invoke('text').then ((txt) => {
-                        if (txt=dashName) {
+                        if (txt=admDashJson[0].type[0]) {
                             cy.log('Category name verified:', txt)
                         }
                         else {}
                     })
 
-            })
-            cy.contains(dashName).click()
+                })
+                cy.contains(admDashJson[0].type[0]).click()
 
     //verify the correct category entered
-            cy.get('.dashlet-selector__sub-title').should('have.text', dashName +" Dashlets")
+                cy.get('.dashlet-selector__sub-title').should('have.text', admDashJson[0].type[0] +" Dashlets")
             
     //Verify the number of items
-            cy.get('.dashlet-selector-item__title').should('have.length', 4)
+                cy.get('.dashlet-selector-item__title').should('have.length', 4)
 
     //Verify the list of dashlet names and descriptions
             //JSON FILE!!!!
-        cy.get("@admDashJson").then((admDashJson) =>{
-            cy.get('.dashlet-selector-item__title').each((item,index) => {
-                cy.wrap(item).should('contain.text', admDashJson[0].name[index]).parent()
-                .should('contain.text', admDashJson[0].descr[index])
-                cy.wrap(item).should('contain.text', admDashJson[0].name[index]).siblings('.dashlet-selector-item__content').invoke('text').then(cy.log)
-            })
+        
+                cy.get('.dashlet-selector-item__title').each((item,index) => {
+                    cy.wrap(item).should('contain.text', admDashJson[0].name[index]).parent()
+                    .should('contain.text', admDashJson[0].descr[index])
+                    cy.wrap(item).should('contain.text', admDashJson[0].name[index]).siblings('.dashlet-selector-item__content').invoke('text').then(cy.log)
+                })
     
-        })
+            })
 
-    });
+        });
     
 
 
--- cypress/integration/Avantra UI/Login_page_UI_Olha.js
+++ cypress/integration/Avantra UI/Login_page_UI_Olha.js
@@ -9,34 +9,40 @@ const login = "Olha_test";
 const password = "Olha1605";
 const incor_login = "Olha_incorrect";
 const incor_password = "Incorrect_pass";
-
+//Checks input fields and eyeball
     it("Login UI without checkbox", () => {
 cy.get('#input-login-id').type(login)
 cy.get('#input-password-id').type(password)
 cy.get('#input-password-id').invoke('attr', 'type').should('contain', 'password')
 cy.get('.input-field__password-visibility-switch').click()
 cy.get('#input-password-id').invoke('attr', 'type').should('contain', 'text')
+cy.get('#input-password-id').invoke('val').should('contain', password)
 cy.get('.background-primary').contains("Login to Avantra").click()
     });
 
+//Tooltips
     it.only("Login and Password fields validation", () => {
 cy.get('.background-primary').contains("Login to Avantra").click()
 cy.wait(500)
+// cy.trigger('mouseover', '[fieldid="input-login-id"]')
+// cy.get('[fieldid="input-login-id"]').should('contain', 'Login should not be empty')
+// realHover()
+
+  // cy.get('#input-login-id').should('have.class', 'ng-invalid')
+// cy.get('[fieldid="input-password-id"]').invoke('attr', 'tooltip').should('contain', 'Password should not be empty')
 
 
-cy.get('#input-login-id').invoke('attr', 'tooltip').should('contain', 'Login should not be empty')
-cy.get('#input-login-id').should('have.class', 'ng-invalid')
-cy.get('[fieldid="input-password-id"]').invoke('attr', 'tooltip').should('contain', 'Password should not be empty')
-cy.get('#input-password-id').should('have.class', 'ng-invalid')
-cy.get('#input-login-id').type("testLogin")
-cy.get('body').click(0,0)
-cy.wait(500)
-cy.get('[fieldid="input-login-id"]').should('have.class', 'ng-valid')
-cy.get('#input-password-id').type("testPassword")
-cy.get('body').click(0,0)
-cy.wait(500)
-cy.get('[fieldid="input-password-id"]').should('have.class', 'ng-valid')
+// cy.get('#input-password-id').should('have.class', 'ng-invalid')
+// cy.get('#input-login-id').type("testLogin")
+// cy.get('body').click(0,0)
+// cy.wait(500)
+// cy.get('[fieldid="input-login-id"]').should('have.class', 'ng-valid')
+// cy.get('#input-password-id').type("testPassword")
+// cy.get('body').click(0,0)
+// cy.wait(500)
+// cy.get('[fieldid="input-password-id"]').should('have.class', 'ng-valid')
     });
+
     it("Response 200", () => {
         cy.get('#input-login-id').type(login)
         cy.get('#input-password-id').type(password)
