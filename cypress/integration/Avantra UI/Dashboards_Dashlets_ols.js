10865a827f2509f171098420add9d94a615a7d3e
Olha <vicontesse@gmail.com>
2022-01-25 15:33:01 +0200

    Asserting_with_JSON

--- cypress/integration/Avantra UI/Dashboards_Dashlets_ols.js
+++ cypress/integration/Avantra UI/Dashboards_Dashlets_ols.js
@@ -1,9 +1,13 @@
 /// <reference types="cypress" />
 
-describe("Dashlets", () => {
+describe("Dashlets and dashboards", () => {
+    before (function(){
+        cy.fixture("Admin_dashlets").as("admDashJson")
+    })
     beforeEach(() => {
             const login = "Olha_test";
             const password = "Olha1605";
+            
         cy.visit ("https://eiger.dev.gcp.avantra.net:8443/xn/ui")
         cy.get('#input-login-id').type(login)
         cy.get('#input-password-id').type(password)
@@ -49,67 +53,48 @@ it("Save the dashboard with dashlet added", () => {
         cy.contains('Yes').click()
     })
 //Verify the dashboard is deleted
-cy.contains('a', 'OLS3').should('not.exist')
+    cy.contains('a', 'OLS3').should('not.exist')
         });
 
 //Dashlets selecting
         it.only("Administrative Dashlets", () => {
+            const dashName = 'Administrative'
             cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text','Dashboards')
             cy.get('.sidebar-list__header > app-button.ng-star-inserted > .icon-button > .icon-button__control > svg').click();
             cy.get('.dashboard-modify__header-input').clear();
-            cy.get('.dashboard-modify__header-input').type('Administrative-ols');
+            cy.get('.dashboard-modify__header-input').type(dashName);
             cy.get('.dashboard-modify__add-dashlet').click();
-            cy.wait(2000)
+            cy.wait(600)
     //Log each element of dashlet categories
             cy.get('[class="dashlet-selector-categories"]').within(() => {
-                cy.get('[class="dashlet-selector-categories__item ng-star-inserted"]').each(($el) =>{
-                    cy.get($el).invoke('text').then(cy.log)
-                })
+                    cy.get('[class="dashlet-selector-categories__item ng-star-inserted"]').invoke('text').then ((txt) => {
+                        if (txt=dashName) {
+                            cy.log('Category name verified:', txt)
+                        }
+                        else {}
+                    })
 
             })
-            cy.contains('Administrative').click()
+            cy.contains(dashName).click()
+
     //verify the correct category entered
-            cy.get('.dashlet-selector__sub-title').should('have.text', "Administrative Dashlets")
+            cy.get('.dashlet-selector__sub-title').should('have.text', dashName +" Dashlets")
+            
     //Verify the number of items
             cy.get('.dashlet-selector-item__title').should('have.length', 4)
-    //Verify the list of dashlet names
-            cy.get('.dashlet-selector-item__title').then(($els) => {
-               return (
-               Cypress.$.makeArray($els)
-               .map((el) => el.innerText)
-               )
-            })
-            .should('deep.equal', ['Signed in Users', 'Check For Updates', 'Logbook Activities', 'Business Service Node'])
-    //Verify the Dashlet description one by one
-                cy.get('.dashlet-selector-item__title').each(($el, index, $list) =>{
-                    if($el.text().includes('Signed in Users')){
-                        cy.get($el).parent().within(() =>{
-                            cy.get('.dashlet-selector-item__content--description-text').contains('Show number of users signed in to Avantra')
-                            .invoke('text').then(cy.log)
-                        })
-                    }
-                    else if($el.text().includes('Check For Updates')){
-                        cy.get($el).parent().within(() =>{
-                            cy.get('.dashlet-selector-item__content--description-text').contains('Show new Avantra updates which are available at syslink download site.')
-                            .invoke('text').then(cy.log)
-                        })
-                    }
-                    else if($el.text().includes('Logbook Activities')){
-                        cy.get($el).parent().within(() =>{
-                            cy.get('.dashlet-selector-item__content--description-text').contains('Show the number of Logbook activities')
-                            .invoke('text').then(cy.log)
-                        })
-                    }
-                    else {
-                        cy.get($el).parent().within(() =>{
-                            cy.get('.dashlet-selector-item__content--description-text').contains('Shows Result of a single node from a Business Service')
-                            .invoke('text').then(cy.log)
-                        })
-                    }
-    
 
+    //Verify the list of dashlet names and descriptions
+            //JSON FILE!!!!
+        cy.get("@admDashJson").then((admDashJson) =>{
+            cy.get('.dashlet-selector-item__title').each((item,index) => {
+                cy.wrap(item).should('contain.text', admDashJson[0].name[index]).parent()
+                .should('contain.text', admDashJson[0].descr[index])
+                cy.wrap(item).should('contain.text', admDashJson[0].name[index]).siblings('.dashlet-selector-item__content').invoke('text').then(cy.log)
             })
-        });
+    
+        })
+
+    });
     
 
 
