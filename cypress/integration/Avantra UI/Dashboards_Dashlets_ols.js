d21dc8a98514dc0cfa3af03c4fdc1748ab1eab32
Olha <vicontesse@gmail.com>
2022-05-23 11:51:28 +0300

    test_all_dashlets reworked

--- cypress/integration/Avantra UI/Dashboards_Dashlets_ols.js
+++ cypress/integration/Avantra UI/Dashboards_Dashlets_ols.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Dashlets and dashboards", () => {
+describe("Dashlets and dashboards", { defaultCommandTimeout: 5000 },() => {
     before(function () {
         cy.fixture("Admin_dashlets").as("admDashJson")
         cy.fixture("Credentials").as("creds")
@@ -21,16 +21,25 @@ describe("Dashlets and dashboards", () => {
         cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text', 'Dashboards')
         cy.get('.sidebar-list__header > .mat-tooltip-trigger > .icon-button > .background-undefined').click();
         cy.get('.dashboard-modify__header-input').clear();
-        cy.get('.dashboard-modify__header-input').type('OLS11');
+        cy.get('.dashboard-modify__header-input').type('OLS13');
         cy.get('.dashboard-modify__add-dashlet').click();
         cy.get(':nth-child(2) > :nth-child(1) > avantra-dashlet-selector-item > .dashlet-selector-item > .dashlet-selector-item__button').click();
         cy.get('.dropdown-group > :nth-child(1) > .mat-tooltip-trigger > .select > #undefined > .ng-select-container > .ng-arrow-wrapper').click();
-        cy.get('.ng-dropdown-panel-items').within(() => {
-            cy.get('.ng-option').contains('BS_REL_8').click()
+        cy.get('.ng-dropdown-panel').within(() => {
+            // cy.get('#ad267ff583a0-6')
+            cy.get('.ng-star-inserted').contains('BS_REL_8').click({force:true})
         })
         cy.wait(3000)
         //get element within another element
         cy.get('.dashlet-add__stepper').within(() => {
+            cy.get('[mattooltip="Save"]')
+            .within(() => {
+                cy.get('.icon-button__text')
+                .click({force:true})
+            })
+        })
+        cy.wait(5000)
+        cy.get('.sub-header').within(() => {
             cy.get('[mattooltip="Save"]')
             .within(() => {
                 cy.get('.icon-button__text')
--- cypress/integration/Avantra UI/Test_all_Dashlets.js
+++ cypress/integration/Avantra UI/Test_all_Dashlets.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Test All dashlets", () => {
+describe("Test All dashlets", { defaultCommandTimeout: 5000 },() => {
     before(function () {
         cy.fixture("Admin_dashlets").as("admDashJson")
         cy.fixture("Credentials").as("creds")
@@ -10,17 +10,15 @@ describe("Test All dashlets", () => {
         // DO NOT FORGET TO USE YOUR CREDS!!!!!!
         cy.get("@creds").then((creds) => {
             cy.visit("https://eiger.dev.gcp.avantra.net:8443/xn/ui")
+            cy.wait (5000)
             cy.get('#input-login-id').type(creds.login)
             cy.get('#input-password-id').type(creds.password)
             cy.get('.background-primary').contains("Login to Avantra").click()
-            // cy.get('.sidebar-list__header').within(() => {
-            //     cy.get('[iconpath="assets/media/icons/shared/plus.svg"]').click()
-            // })
-            // cy.get('.dashboard-modify__add-dashlet').click();
+
         })
     })
 
-    it.only("Business Service Node", () => {
+    it("Business Service Node", () => {
         cy.get(':nth-child(2) > :nth-child(1) > avantra-dashlet-selector-item > .dashlet-selector-item > .dashlet-selector-item__button').click();
         cy.get('.dropdown-group > :nth-child(1) > .mat-tooltip-trigger > .select > #undefined > .ng-select-container > .ng-arrow-wrapper').click();
         cy.get('.ng-dropdown-panel-items').within(() => {
@@ -28,11 +26,15 @@ describe("Test All dashlets", () => {
             cy.get('.header__edit-block > .mat-tooltip-trigger.ng-star-inserted > .icon-button > .background-primary').click()
         })
     })
-// BUG #FEA-423
+//
     it("Logbook Activities", () => {
+        cy.get('*[class="sidebar-list__title ng-star-inserted"]').should('have.text', 'Dashboards')
+        cy.get('.sidebar-list__header > .mat-tooltip-trigger > .icon-button > .background-undefined').click();
+        cy.get('.dashboard-modify__header-input').clear();
+        cy.get('.dashboard-modify__add-dashlet').wait(2000).click();
         cy.get('.dashlet-selector-item__title').contains('Logbook Activities').parent()
         .within (() =>{
-            cy.get('.dashlet-selector-item__button').click()
+            cy.get('.dashlet-selector-item__button').wait(2000).click()
         })
         cy.get('[placeholder="Logbook Activities"]').type("Logbook_ols")
         cy.get('[formcontrolname="subtitle"]').children('input').type("Autotest")
@@ -51,18 +53,29 @@ describe("Test All dashlets", () => {
         cy.wait(600)
         cy.get('.sidebar-list-item').contains('a', " Logbook_act_dash_test ")
             .click()
-        cy.get('.sub-header')
-            .get('.edit-button-with-text')
-                .click()
-        cy.get('.dashboard-modify__add-dashlet')
-            .click()
-            cy.get('.dashlet-selector-item__title').contains('Logbook Activities').parent()
+        cy.get('.header__edit-block')
+            .get('[mattooltip="Edit Dashboard"]')
+                .wait(2000)
+                .click() 
+                cy.wait(2000)
+        cy.get('.avantra-drawer__content').within (() =>{
+                cy.get('.avantra-dashlet__header')
             .within (() =>{
-                cy.get('.dashlet-selector-item__button').click()
+            cy.get('[mattooltip="Dashlet Settings"]')
+            .click()
             })
-            cy.get('[placeholder="Logbook Activities"]').type("Logbook_ols")
-            cy.get('[formcontrolname="subtitle"]').children('input').type("Autotest")
+        })
+            // cy.get('.dashlet-selector-item__title').contains('Logbook Activities').parent()
+            // .within (() =>{
+            //     cy.get('.dashlet-selector-item__button').wait(2000).click()
+            // })
+            cy.get('[placeholder="Logbook Activities"]').type("Logbook_ols_1")
+            cy.get('[formcontrolname="subtitle"]').children('input').type("Autotest1")
             cy.wait(600)
+            cy.get('.dashlet-settings__param').contains("Refresh Interval").children('[role="combobox"]').click()
+            cy.get('.ng-dropdown-panel-items scroll-host'). within(() => {
+                cy.get('.ng-star-inserted').contains('10 minutes').click()
+            })
             cy.get('.dashlet-add__stepper').within(() => {
                 cy.get('[iconpath="assets/media/icons/shared/menu-ok.svg"]').click()
             })
